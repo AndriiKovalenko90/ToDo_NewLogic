b25a9c3be13040e3a66c2c3339cc3f355bc95d2c
AndriiKovalenko90 <108003741+AndriiKovalenko90@users.noreply.github.com>
2023-07-25 00:37:04 +0300

    -Added FileManager -Added TodoListsmanager

---
+++ Managers/FileManager.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using ToDo_NewLogic.Models;
+
+namespace ToDo_NewLogic.Managers
+{
+    internal class FileManager
+    {
+        public BindingList<Task> LoadTasksFromFile(string filePath)
+        {
+            //Loadtasks from the specified file (filePath and return tham)
+            string json = File.ReadAllText(filePath);
+            BindingList<Task> tasks = JsonConvert.DeserializeObject<BindingList<Task>>(json);
+            return tasks;
+        }
+
+        public void SaveTasksToFile(string filePath, BindingList<Task> tasks)
+        {
+            // Save the provided tasks to the specified file(filePath)
+            string json = JsonConvert.SerializeObject(tasks);
+            File.WriteAllText(filePath, json);
+        }
+
+        public List<TodoList> LoadTodoListsFromFile(string filePath)
+        {
+            List<TodoList> todoLists= new List<TodoList>();
+            if (File.Exists(filePath))
+            {
+                string jsonTodoLists = File.ReadAllText(filePath);
+                todoLists = JsonConvert.DeserializeObject<List<TodoList>>(jsonTodoLists);
+            }
+            return todoLists;
+        }
+
+        public void SaveTodoListsToFile(string filePath, List<TodoList> todoLists)
+        {
+            string jsonTodoLists = JsonConvert.SerializeObject(todoLists);
+            File.WriteAllText(filePath, jsonTodoLists);
+        }
+    }
+}
---
+++ Managers/TodoListManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo_NewLogic.Models;
+
+namespace ToDo_NewLogic.Managers
+{
+    internal class TodoListManager
+    {
+        private List<TodoList> todoLists = new List<TodoList>();
+
+        public List<string> GetRecentTodoLists()
+        {
+            return todoLists.Select(x => x.Title).ToList();
+        }
+
+        public void LoadTodoLists()
+        {
+            FileManager fileManager = new FileManager();
+            todoLists = fileManager.LoadTodoListsFromFile("todoLists.json");
+            
+        }
+
+        public void SaveTodoLists()
+        {
+            FileManager filemanager = new FileManager();
+            filemanager.SaveTodoListsToFile("todoLists.json", todoLists);
+        }
+    }
+}
